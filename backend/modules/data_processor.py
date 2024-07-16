3ab242578870a50884a0320ec0e58c6d31d97084
abahde <71208362+arvedb@users.noreply.github.com>
2024-07-16 20:44:33 +0200

    added docstrings

--- backend/modules/data_processor.py
+++ backend/modules/data_processor.py
@@ -4,7 +4,15 @@ import openfoodfacts
 import product_filters
 
 def filter_product(product: dict, product_filters: list) -> dict:
-    
+    """Filters product based on the provided "product_filters" dict.
+
+    Args:
+        product (dict): Single producct dictionary.
+        product_filters (list): Dictionary of product data, that should be kept.
+
+    Returns:
+        dict: A new copy of the filtered product dictionary.
+    """
     product_copy = product.copy()
     data_filtered = {}
     
@@ -19,20 +27,48 @@ def filter_product(product: dict, product_filters: list) -> dict:
     
     return product_copy
  
-def product_data(product: dict, data_name: str) -> dict:
-    return product[data_name]   
+def product_data(product: dict, data_key: str) -> dict:
+    """Extracts single data of the product like e.g. "nutriments" as dictionary.
 
-def product_data_dataframe(product: dict, data_name: str) -> pd.DataFrame:
+    Args:
+        product (dict): Single producct dictionary.
+        data_key (str): Key of data to extract.
+
+    Returns:
+        dict: Dictionary of desired product data specified by "data_key".
+    """
+    return product[data_key]   
+
+def product_data_dataframe(product: dict, data_key: str) -> pd.DataFrame:
+    """Extracts single data of the product like e.g. "nutriments" as pandas DataFrame .
+
+    Args:
+        product (dict): Single producct dictionary.
+        data_key (str): Key of data to extract.
+
+    Returns:
+        pd.DataFrame: DataFrame of desired product data specified by "data_key".
+    """
     dataframe = pd.DataFrame()
-    for key, value in product[data_name].items():
+    for key, value in product[data_key].items():
         print(key, value)
         dataframe[key] = [value]
     return dataframe
 
 def product(response, index: int = 0) -> dict:
+    """Extract a single product from the response specified by "index".
+    If the response contains only one product, the single product will be returned.
+
+    Args:
+        response (_type_): Response dictionary of API Call.
+        index (int, optional): Index number of product to extract. Counting from 1.
+
+    Returns:
+        dict: Dictionary of single product.
+    """
     
     if 'count' in response.keys():
-        return response['products'][index+1]
+        return response['products'][index-1]
     else:
         return response
 
