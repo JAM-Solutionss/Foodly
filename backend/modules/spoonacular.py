6933fecda5b115807e7f5e7fcecdc47ef3d65118
Cipher Shadow <mprogrammierer@gmail.com>
2024-07-16 10:38:48 +0200

    json output

--- backend/modules/spoonacular.py
+++ backend/modules/spoonacular.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -8,32 +9,36 @@ def get_recipes(ingredients, number=10, ranking=1, ignore_pantry=True):
     
     url = 'https://api.spoonacular.com/recipes/findByIngredients'
     api_key = os.getenv('SPOONACULAR_API_KEY')
-    #print(api_key)
 
     params = {
         'apiKey': api_key,
         'ingredients': ingredients_str,
-        'number': number,
-        'ranking': ranking,
-        'ignorePantry': ignore_pantry
+        'number': number,  # Number of recipes to retrieve
+        'ranking': ranking,  # 1 = popularity, 2 = healthier
+        'ignorePantry': ignore_pantry  # True = only our ingredients, False = allow other ingredients
     }
     
     try:
+        # Send API request
         response = requests.get(url, params=params)
         
         if response.status_code == 200:
             recipes = response.json()
-            
-            for recipe in recipes:
-                print(f"Rezept: {recipe.get('title', 'Nicht verfügbar')}")
-                print(f"ID: {recipe.get('id', 'Nicht verfügbar')}")
-                print(f"Bild: {recipe.get('image', 'Nicht verfügbar')}")
-                print("-----------------------")
+
+            # Check if recipes.json already exists
+            file_path = 'recipes.json'
+            if not os.path.exists(file_path):
+                with open(file_path, 'w', encoding='utf-8') as f:
+                    json.dump(recipes, f, ensure_ascii=False, indent=4)
+                print(f'Created new file {file_path}')
+            else:
+                print(f'{file_path} already exists. Skipping creation.')
+
         else:
-            print(f"Fehler bei der Anfrage. Status-Code: {response.status_code}")
+            print(f'Error in request. Status code: {response.status_code}')
             
     except requests.exceptions.RequestException as e:
-        print(f"Fehler bei der Anfrage: {e}")
+        print(f'Error in request: {e}')
 
 
 ingredients_list = ['chicken', 'rice', 'broccoli']
