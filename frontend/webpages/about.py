c76c1a70361fda029a6f36635e3a5407bb44959e
mirixy <miriam.lucy@outlook.com>
2024-07-17 23:01:51 +0200

    added GitHub Links

--- frontend/webpages/about.py
+++ frontend/webpages/about.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("What is FoodFacts?")
 st.write("FoodFacts is a web application that uses OpenFoodFacts API to display nutritional information about a product.")
 st.subheader("Our Team:")
-st.subheader("Our Team:")
 st.markdown("""
 <p align="center">
 <a href="https://github.com/cipher-shad0w" target="_blank">Cipher Shadow</a> &#xa0; | &#xa0;
