b8cf46e5c12c7b87cc78cd8cd4de37c374667af4
mirixy <miriam.lucy@outlook.com>
2024-07-17 22:09:19 +0200

    changed init

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,2 +1,3 @@
-from .modules import *
-from .main import *
+from .modules.data_processor import product, filter_nutriments, nutriments, nutriments_dataframe, nutrigrade, nutriscore, get_multiple_products
+from .modules.product_filters import nutriments_filters
+from .modules.spoonacular import get_recipes
