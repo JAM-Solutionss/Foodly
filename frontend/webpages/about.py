315adf29a007bb79ee8ef229c20a5c3f2d540b78
mirixy <miriam.lucy@outlook.com>
2024-07-17 12:13:59 +0200

    Changed About page butt needs probabbly more details.

--- frontend/webpages/about.py
+++ frontend/webpages/about.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("What is FoodGPT?")
+st.title("What is FoodFacts and FoodGPT?")
 st.write("FoodGPT gets the ingredients for a meal from a Dataset and uses a Language Model to generate a meal.")
+st.write("FoodFacts is a web application that uses OpenFoodFacts API to display nutritional information about a product.")
 st.subheader("Our Team:")
 st.write("Arved, Mister Spies, miri_the_creative | Miriam")
